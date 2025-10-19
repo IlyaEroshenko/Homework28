fcb330ed69c8ff63e4f4037975639f56ddbda256
Ily <ilyeroshenko@gmail.com>
2025-10-19 15:20:33 +0700

    код послепроверки flake8, 1 ошибка

--- src/masks.py
+++ src/masks.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+
 def get_mask_card_number(card_number: str) -> str:
     """Функция принимает на вход номер карты и возвращает ее маску."""
     if len(card_number) == 16:  # Проверяем количество цифр в номере
@@ -22,9 +23,11 @@ def get_date(date_string: str, date_format="%Y-%m-%dT%H:%M:%S.%f") -> str:
         return None
 
 
-
-
- # print() используется в данном коде только для вызова функции, при дальнейшей работе он будет удалён.
+        # print() используется в данном коде только для вызова функции, при дальнейшей работе он будет удалён.
 print(get_mask_card_number("7000792289606361"))
+
+
 print(get_mask_account("73654108430135874305"))
+
+
 print(get_date("2024-03-11T02:26:18.671407"))
