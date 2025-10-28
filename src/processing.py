a337c67bcde5e2b285e3acd57729e71f79580a37
Ily <ilyeroshenko@gmail.com>
2025-10-28 23:23:38 +0700

    часть дз готова

--- src/processing.py
+++ src/processing.py
@@ -1,28 +1,32 @@
-def filter_by_state(data: list, state: str='EXECUTED'):
+def filter_by_state(data: list, state: str = 'EXECUTED') -> list:
     """
      Пишем функцию, которая принимает список словарей и
       возвращает новый список словарей, содержащий только те словари,
       у которых ключ соответствует указанному значению.
     """
 # Сщздаём новый список для отфильтрованых элементов
-filtered_data = []
+    filtered_data = []
+
+
+# Перебераем все данные
+    for item in data:
+        if item.get("state") == state:
+            filtered_data.append(item)
+
+    return filtered_data
+
+
 transactions = [
     {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
     {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
     {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
-    ]
-
-# Перебераем все данные
-for item in data:
-    if item.get("state") == state:
-        filtered_data.append(item)
-return filtered_data
+    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
 
 
+# Вывод функции со статусом по умолчанию 'EXECUTED'
+result_executed = filter_by_state(transactions)
+print(result_executed)
 
-# Вызов функции filter_by_state
-result = filter_by_state(transactions)
-
-# Печать результата
-print(result)
+# Вывод функции, если вторым аргументом передано 'CANCELED'
+result_canceled = filter_by_state(transactions, 'CANCELED')
+print(result_canceled)
