6e38c017dc848db812a0f6190bf2ad7c87cce307
Ily <ilyeroshenko@gmail.com>
2025-10-29 22:02:22 +0700

    написал первый вариант функции sort_by_data

--- src/processing.py
+++ src/processing.py
@@ -30,3 +30,32 @@ print(result_executed)
 # Вывод функции, если вторым аргументом передано 'CANCELED'
 result_canceled = filter_by_state(transactions, 'CANCELED')
 print(result_canceled)
+
+
+from datetime import datetime
+
+def sort_by_date(list_of_dicts: list, descending = True):
+    """
+    Пишем функцию, которая принимает список словарей и необязательный параметр,
+    задающий порядок сортировки (по умолчанию — убывание).
+    """
+
+    key_list = []
+
+    for item in list_of_dicts:
+        if descending == True:
+            key_list.append(item)
+
+    return key_list
+
+
+# Пример использования:
+data = [
+    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+]
+
+sort_result = sort_by_date(data)
+print(sort_result)
