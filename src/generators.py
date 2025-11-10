e9a4fda3111514249790ed4d53f8f3a7e2ad6512
Ily <ilyeroshenko@gmail.com>
2025-11-10 22:05:02 +0700

    написал вторую функцию

--- src/generators.py
+++ src/generators.py
@@ -1,13 +1,14 @@
+from transactions_data import transactions
+
+
 def filter_by_currency(transactions, currency):
     """
         Фильтрует список транзакций и возвращает транзакции, соответствующие заданной валюте.
         Использует генератор для эффективной обработки данных.
-
         Args:
             transactions: Список словарей, где каждый словарь представляет транзакцию
                           и содержит ключ 'currency', определяющий валюту операции.
             currency: Валюта, по которой нужно отфильтровать транзакции (строка).
-
         Yield:
             Словари, представляющие транзакции, у которых валюта (currency) совпадает с заданной.
         """
@@ -19,50 +20,27 @@ def filter_by_currency(transactions, currency):
         except (KeyError, TypeError):
             continue  # Если структура словаря не соответствует ожидаемой, игнорируем транзакцию.
 
-transactions = [{
-          "id": 939719570,
-          "state": "EXECUTED",
-          "date": "2018-06-30T02:08:58.425572",
-          "operationAmount": {
-              "amount": "9824.07",
-              "currency": {
-                  "name": "EUR",
-                  "code": "EUR"
-              }
-          },
-          "description": "Перевод организации",
-          "from": "Счет 75106830613657916952",
-          "to": "Счет 11776614605963066702"
-      }, {
-          "id": 939719580,
-          "state": "EXECUTED",
-          "date": "2019-06-30T02:08:58.425572",
-          "operationAmount": {
-              "amount": "9829.04",
-              "currency": {
-                  "name": "RUB",
-                  "code": "RUB"
-              }
-          },
-          "description": "Перевод организации",
-          "from": "Счет 75106830613657916952",
-          "to": "Счет 11776614605963066702"
-      },
-      {
-              "id": 142264268,
-              "state": "EXECUTED",
-              "date": "2019-04-04T23:20:05.206878",
-              "operationAmount": {
-                  "amount": "79114.93",
-                  "currency": {
-                      "name": "USD",
-                      "code": "USD"
-                  }
-              },
-              "description": "Перевод со счета на счет",
-              "from": "Счет 19708645243227258542",
-              "to": "Счет 75651667383060284188"
-       }]
 rub_transactions = filter_by_currency(transactions, "RUB")
 for transaction in rub_transactions:
     print(transaction)
+
+
+def transaction_descriptions(transactions):
+    """
+    Генератор, извлекающий описание каждой транзакции из списка словарей.
+    Args:
+        transactions: Список словарей, где каждый словарь представляет транзакцию
+                      и содержит ключ 'description' с описанием операции.
+    Yields:
+        Описание транзакции (значение ключа 'description').
+    """
+    for transaction in transactions:
+        try:
+            description = transaction['description']  # Пытаемся получить описание
+            yield description  # Возвращаем описание текушей транзакции.
+        except (KeyError, TypeError): # Обрабатываем случаи, если ключа 'description' нет в словаре
+            continue
+
+descriptions = transaction_descriptions(transactions)
+for description in descriptions:
+    print(description)
